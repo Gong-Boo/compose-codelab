845de9901b61a43cac2ddfa4fda0c5bc1d113b31
Jin Park <parkjin0318@naver.com>
2022-08-23 01:14:48 +0900

    update: main state hoisting

--- Jetpack Compose 기초/parkjin/app/src/main/java/com/parkjin/compose/foundation/MainActivity.kt
+++ Jetpack Compose 기초/parkjin/app/src/main/java/com/parkjin/compose/foundation/MainActivity.kt
@@ -3,9 +3,12 @@ package com.parkjin.compose.foundation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -15,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposefoundationTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -34,9 +38,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(vararg names: String = arrayOf("hello", "compose")) {
-    Column {
-        names.forEach { name ->
+fun MyApp() {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(onContinueClick = {
+            shouldShowOnBoarding = false
+        })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnBoardingScreen(onContinueClick: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClick
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
             Greeting(name = name)
         }
     }
@@ -77,6 +113,16 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposefoundationTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingScreenPreview() {
+    ComposefoundationTheme {
+        OnBoardingScreen {
+            /* TODO */
+        }
     }
 }
