296e499afb94d341075288a5d4722faa0aefb36c
Jin Park <parkjin0318@naver.com>
2022-08-23 01:03:52 +0900

    update: main compose state

--- Jetpack Compose 기초/parkjin/app/src/main/java/com/parkjin/compose/foundation/MainActivity.kt
+++ Jetpack Compose 기초/parkjin/app/src/main/java/com/parkjin/compose/foundation/MainActivity.kt
@@ -3,9 +3,18 @@ package com.parkjin.compose.foundation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,22 +44,30 @@ fun MyApp(vararg names: String = arrayOf("hello", "compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = if (expanded) 40.dp else 0.dp)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
 
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
+            OutlinedButton(
+                onClick = {
+                    expanded = expanded.not()
+                }
+            ) {
+                Text(
+                    text = if (expanded) "Show Less" else "Show More"
+                )
             }
         }
     }
