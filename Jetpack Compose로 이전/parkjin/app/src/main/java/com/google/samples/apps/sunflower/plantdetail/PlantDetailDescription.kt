709f0ce2fb22d85ece88a71196a03474aad16ba1
박진 <parkjin0318@naver.com>
2022-08-23 18:44:44 +0900

    update: main viewModel & liveData

--- Jetpack Compose로 이전/parkjin/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ Jetpack Compose로 이전/parkjin/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -20,14 +20,40 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
+
+@Composable
+fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
+    val plant by viewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(plant = it)
+    }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    PlantName(name = plant.name)
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme {
+        PlantDetailContent(plant)
+    }
+}
 
 @Composable
 private fun PlantName(name: String) {
@@ -45,12 +71,5 @@ private fun PlantName(name: String) {
 private fun PlantNamePreview() {
     MaterialTheme {
         PlantName(name = "Apple")
-    }
-}
-
-@Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
     }
 }
