3a08ee386a3774d5f85cf9a6b44fb240bd4dccbd
Jin Park <parkjin0318@naver.com>
2022-08-30 01:56:47 +0900

    remove: main 주석

--- Compose의 기본 레이아웃/parkjin/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ Compose의 기본 레이아웃/parkjin/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.codelab.basiclayouts
 
 import android.os.Bundle
@@ -213,7 +197,6 @@ fun HomeSection(
     }
 }
 
-// Step: Home screen - Scrolling
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
@@ -233,7 +216,6 @@ fun HomeScreen(modifier: Modifier = Modifier) {
     }
 }
 
-// Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
     BottomNavigation(
