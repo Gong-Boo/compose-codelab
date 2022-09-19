c4e4e13e352b642b4450ce883e9ef008a2724175
Jin Park <parkjin0318@naver.com>
2022-09-20 00:16:10 +0900

    update: main hoisting state

--- Jetpack Compose의 상태/parkjin/app/src/main/java/com/parkjin/compose/state/MainActivity.kt
+++ Jetpack Compose의 상태/parkjin/app/src/main/java/com/parkjin/compose/state/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,24 +46,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier = modifier)
+    StatefulCounter(modifier = modifier)
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    Column {
+        StatelessCounter(
+            modifier = modifier,
+            count = count,
+            onIncrement = { count++ }
+        )
+        StatelessCounter(
+            modifier = modifier,
+            count = count,
+            onIncrement = { count *= 2 }
+        )
+    }
+}
+
+@Composable
+fun StatelessCounter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    onIncrement: () -> Unit
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            Text(text = "You've had $count glasses")
+            Text("You've had $count glasses.")
         }
-
         Button(
-            onClick = { count++ },
+            onClick = onIncrement,
             modifier = Modifier.padding(top = 8.dp),
             enabled = count < 10
         ) {
-            Text(text = "Add one")
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "Close"
+            )
         }
     }
 }
