79acb8ec05abb7d0761106a99714db17434d85e7
juyeop03 <kjy031104@gmail.com>
2022-09-26 23:11:41 +0900

    add: 부수효과 프로젝트 추가

---
+++ Jetpack Compose의 상태 및 부수 효과에 관한 고급 개념/groo/AdvancedStateAndSideEffectsCodelab-Groo/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.samples.crane.R
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
+
+private const val SplashWaitTime: Long = 2000
+
+@Composable
+fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
+        // TODO: Make LandingScreen disappear after loading data
+        // rememberUpDatedState로 onTimeout을 한번 캡처 해두면 onTimeout이 변경되더라도 영향을 미치지 않는다
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // 컴포저블 내에 위치해야 사용할 수 있다
+        // rememberCoroutineScope를 사용했으면 매번 코루틴이 생성됐을 것이다
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+
+        Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
+    }
+}
