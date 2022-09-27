919ab09958a7edcf31bd24c47d369e4af8ba93ec
박진 <parkjin0318@naver.com>
2022-09-27 18:34:49 +0900

    delete: main import

--- Jetpack Compose의 상태 및 부수 효과에 관한 고급 개념/parkjin/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
+++ Jetpack Compose의 상태 및 부수 효과에 관한 고급 개념/parkjin/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -45,7 +46,6 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest.Builder
-import com.google.accompanist.insets.navigationBarsPadding
 import kotlinx.coroutines.launch
 
 @Composable
