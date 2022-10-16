b0e42c3fbff89c5ee4b499cf8a758a896ea4c3f5
Jin Park <parkjin0318@naver.com>
2022-10-16 23:58:49 +0900

    add: main testing sync

--- Jetpack Compose에서 테스트/parkjin/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ Jetpack Compose에서 테스트/parkjin/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,5 +1,8 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onRoot
@@ -14,7 +17,7 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun myTest() {
+    fun 현재_화면의_라벨과_일치하는가() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
@@ -24,10 +27,15 @@ class TopAppBarTest {
             )
         }
 
-        // composeTestRule.onRoot().printToLog("currentLabelExists")
+        /*
+        composeTestRule.onRoot(useUnmergedTree = true)
+            .printToLog("currentLabelExists")
+         */
 
-        composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertExists()
+        composeTestRule.onNode(
+            matcher = hasText(RallyScreen.Accounts.name.uppercase()) and
+                    hasParent(hasContentDescription(RallyScreen.Accounts.name)),
+            useUnmergedTree = true
+        ).assertExists()
     }
 }
