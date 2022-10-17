fa35b83824bf4ca4f219064a367c0c6aac04eb8a
Jin Park <parkjin0318@naver.com>
2022-10-17 23:09:40 +0900

    update: main label click

--- Jetpack Compose의 접근성/parkjin/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ Jetpack Compose의 접근성/parkjin/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -61,7 +61,11 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
     Row(
-        Modifier.clickable { navigateToArticle(post.id) }
+        Modifier.clickable(
+            onClickLabel = stringResource(id = R.string.action_read_article)
+        ) {
+            navigateToArticle(post.id)
+        }
     ) {
         Image(
             painter = painterResource(post.imageThumbId),
@@ -139,8 +143,12 @@ fun PostCardPopular(
 ) {
     Card(
         shape = MaterialTheme.shapes.medium,
-        modifier = modifier.size(280.dp, 240.dp),
-        onClick = { navigateToArticle(post.id) }
+        modifier = modifier.size(280.dp, 240.dp)
+            .clickable(
+                onClickLabel = stringResource(id = R.string.action_read_article)
+            ) {
+                navigateToArticle(post.id)
+            }
     ) {
         Column {
 
